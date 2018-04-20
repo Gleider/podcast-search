5fa55071583e81126e097f267307926546380f82
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-04-20 09:40:53 -0300

    creating all builds of webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,18 @@
+import {join} from 'path'
+
+const include = join(__dirname, 'src')
+
+export default {
+  entry: './index',
+  output:{
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {test:/\.js$/, use: 'babel-loader'},
+    ]
+  }
+}
