75a4755b01ae977fe8723fb1a13104d17159c05e
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-04-20 10:45:42 -0300

    add exemples to test api

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,7 +7,7 @@ export default {
   output:{
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'spotifyWrapper',
+    library: 'podcastSearch',
   },
   devtool: 'source-map',
   module: {
